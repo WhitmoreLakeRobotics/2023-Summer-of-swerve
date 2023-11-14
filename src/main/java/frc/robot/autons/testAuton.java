70f4e6f1dc0956fbc931d7ac0266350bd5d27732
TroBots <whitmore.robot.frc@outlook.com>
2023-11-14 13:49:09 -0500

    jas format of files

--- src/main/java/frc/robot/autons/testAuton.java
+++ src/main/java/frc/robot/autons/testAuton.java
@@ -3,13 +3,19 @@ package frc.robot.autons;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.driveCommands.cmdDriveStraight;
 import frc.robot.commands.driveCommands.cmdStop;
+import frc.robot.commands.util.cmdDelay;
 
 public class testAuton extends SequentialCommandGroup {
     
     public testAuton() {
         addCommands(new cmdDriveStraight(24, 0.3, 0));
         addCommands(new cmdStop());
+        addCommands(new cmdDelay(1.0));
+        // once the command can drive forward 24 inches then return 
+        // to the starting spot by driving backwards 24 inches
+        
         //addCommands(new cmdDriveStraight(24, -0.3, 0));
         //addCommands(new cmdStop());
+        //addCommands(new cmdDelay(1.0));
     }
 }
